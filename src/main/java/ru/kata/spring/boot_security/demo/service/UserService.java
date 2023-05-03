99b27d8c1faeeaefe69641a495d498e74d98dae9
Nikolay Rastoropov <Nikolay>
2023-05-03 17:21:38 +0300

    3

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,11 +1,10 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
-@Component
+
 public interface UserService {
 
     void saveUser(User user);
